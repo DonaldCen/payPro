f43f5cc6e93a4fdaf1d55dee0af70c5326871c91
zengbin2018 <490086561@qq.com>
2019-03-29 21:28:02 +0800

    s

--- src/main/java/yx/pay/system/service/MerchantApplyService.java
+++ src/main/java/yx/pay/system/service/MerchantApplyService.java
@@ -3,6 +3,8 @@ package yx.pay.system.service;
 import yx.pay.common.service.IService;
 import yx.pay.system.domain.wx.MerchantApply;
 
+import java.util.List;
+
 /**
  * @Description
  * @Author <a href="mailto:cenyingqiang@wxchina.com">yingqiang.Cen</a>
@@ -10,4 +12,5 @@ import yx.pay.system.domain.wx.MerchantApply;
  * @Version 1.0.0
  */
 public interface MerchantApplyService extends IService<MerchantApply> {
+    List<MerchantApply> findMerchantApplyList();
 }
