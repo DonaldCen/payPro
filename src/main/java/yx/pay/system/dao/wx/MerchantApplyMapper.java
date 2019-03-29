58ecf3e7e7c437e0addff725ba14a75a82c85d8c
zengbin2018 <490086561@qq.com>
2019-03-29 20:43:02 +0800

    addMerchantApply,updateMerchantApply

--- src/main/java/yx/pay/system/dao/wx/MerchantApplyMapper.java
+++ src/main/java/yx/pay/system/dao/wx/MerchantApplyMapper.java
@@ -14,8 +14,18 @@ import java.util.List;
 public interface MerchantApplyMapper extends MyMapper<MerchantApply> {
     List<MerchantApply> findChannelList(MerchantApply merchantApply);
     MerchantApply findByName(String businessCode);
-    void addMerchantApply(MerchantApply merchantApply);
-    void updateMerchantApply(MerchantApply merchantApply);
+    /**
+     * 新增商户入驻申请参数
+     * @param paramMap
+     */
+    void addMerchantApply(java.util.Map paramMap);
+    /**
+     * 更新申请入驻结果（成功，失败）
+     * @param paramMap
+     */
+    void updateMerchantApply(java.util.Map paramMap);
+    Long getMerchantApplyNextID();
+
     int updateSubMchIdAndStatus(MerchantApply merchantApply);
     int updateStatus(MerchantApply merchantApply);
 }
